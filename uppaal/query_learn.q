7e63fc1fb32396b31ea3dc40b937734ad6325e92
rgu01 <rong.gu@mdu.se>
2024-05-16 15:47:49 +0200

    run_verify_mac

---
+++ uppaal/query_learn.q
@@ -0,0 +1,2 @@
+strategy reach = minE(D) [<=MAXTIME]: <> time>=MAXTIME
+simulate [<=MAXTIME] { varsEgo.x, varsEgo.y, varsEgo.orientation, varsEgo.velocity, varsEgo.acceleration } under reach
--- uppaal/run_verifyta_mac.sh
+++ uppaal/run_verifyta_mac.sh
@@ -1,1 +1,1 @@
-./bin_mac/verifyta ./models/DEU_Ffb-1_3_T-1_generated_lanelet.xml query.q -F 1.0 -D 0.01 -s --max-iterations 1 --eval-runs 1 --good-runs 100 --total-runs 200 --reset-no-better 1 --max-reset-learning 1
+./bin_mac/verifyta ./models/DEU_Ffb-1_3_T-1_generated_lanelet.xml query_learn.q -F 1.0 -D 0.01 -s --max-iterations 1 --eval-runs 1 --good-runs 100 --total-runs 200 --reset-no-better 1 --max-reset-learning 1

cc04f119a99e0f4de557be4ad735dd5200514f05
rgu01 <rong.gu@mdu.se>
2024-05-17 11:39:10 +0200

    gu: exp1

--- experiments/query_exp1.q
+++ experiments/query_exp1.q
@@ -2,4 +2,3 @@ A[] !dStateEgo.detection.collide && !dStateEgo.detection.outside
 E[] !dStateEgo.detection.collide && !dStateEgo.detection.outside
 strategy safe = control:A[] !dStateEgo.detection.collide && !dStateEgo.detection.outside
 strategy reachSafe = minE(D) [<=MAXTIME] {egoController.location}->{dStateEgo.position.x, dStateEgo.position.y, dStateEgo.velocity, dStateEgo.orientation, dStateEgo.acceleration, dStateEgo.yawRate}: <> time>=MAXTIME under safe
-simulate [<=MAXTIME] { varsEgo.x, varsEgo.y, varsEgo.orientation, varsEgo.velocity, varsEgo.acceleration } under reachSafe
--- experiments/run_exp1.sh
+++ experiments/run_exp1.sh
@@ -1,1 +1,20 @@
-../uppaal/bin_mac/verifyta ../uppaal/models/ZAM_Tutorial-1_2_T-1_generated_lanelet.xml query_exp1.q -F 1.0 -D 0.01 -s --max-iterations 1 --eval-runs 1 --good-runs 100 --total-runs 200 --reset-no-better 1 --max-reset-learning 1 >> result_exp1.log
+echo "SCENARIO: ZAM_Tutorial-1_2_T-1\n" >> result_exp1.log
+../uppaal/bin_mac/verifyta ../uppaal/models/ZAM_Tutorial-1_2_T-1_generated_lanelet.xml query_exp1.q -u -D 0.01 -s --max-iterations 1 --eval-runs 1 --good-runs 100 --total-runs 200 --reset-no-better 1 --max-reset-learning 1 >> result_exp1.log
+echo "\nSCENARIO: ZAM_Tjunction-1_216_T-1\n" >> result_exp1.log
+../uppaal/bin_mac/verifyta ../uppaal/models/ZAM_Tjunction-1_216_T-1_generated_lanelet.xml query_exp1.q -u -D 0.01 -s --max-iterations 1 --eval-runs 1 --good-runs 100 --total-runs 200 --reset-no-better 1 --max-reset-learning 1 >> result_exp1.log
+echo "\nSCENARIO: ZAM_Ramp-1_1-T-1\n" >> result_exp1.log
+../uppaal/bin_mac/verifyta ../uppaal/models/ZAM_Ramp-1_1-T-1_generated_lanelet.xml query_exp1.q -u -D 0.01 -s --max-iterations 1 --eval-runs 1 --good-runs 100 --total-runs 200 --reset-no-better 1 --max-reset-learning 1 >> result_exp1.log
+echo "\nSCENARIO: ZAM_Over-1_1\n" >> result_exp1.log
+../uppaal/bin_mac/verifyta ../uppaal/models/ZAM_Over-1_1_generated_lanelet.xml query_exp1.q -u -D 0.01 -s --max-iterations 1 --eval-runs 1 --good-runs 100 --total-runs 200 --reset-no-better 1 --max-reset-learning 1 >> result_exp1.log
+echo "\nSCENARIO: USA_US101-1_1_T-1\n" >> result_exp1.log
+../uppaal/bin_mac/verifyta ../uppaal/models/USA_US101-1_1_T-1_generated_lanelet.xml query_exp1.q -u -D 0.01 -s --max-iterations 1 --eval-runs 1 --good-runs 100 --total-runs 200 --reset-no-better 1 --max-reset-learning 1 >> result_exp1.log
+echo "\nSCENARIO: DEU_Muc-11_1_T-1\n" >> result_exp1.log
+../uppaal/bin_mac/verifyta ../uppaal/models/DEU_Muc-11_1_T-1_generated_lanelet.xml query_exp1.q -u -D 0.01 -s --max-iterations 1 --eval-runs 1 --good-runs 100 --total-runs 200 --reset-no-better 1 --max-reset-learning 1 >> result_exp1.log
+echo "\nSCENARIO: DEU_Muc-3_1_T-1\n" >> result_exp1.log
+../uppaal/bin_mac/verifyta ../uppaal/models/DEU_Muc-3_1_T-1_generated_lanelet.xml query_exp1.q -u -D 0.01 -s --max-iterations 1 --eval-runs 1 --good-runs 100 --total-runs 200 --reset-no-better 1 --max-reset-learning 1 >> result_exp1.log
+echo "\nSCENARIO: DEU_Hhr-1_1\n" >> result_exp1.log
+../uppaal/bin_mac/verifyta ../uppaal/models/DEU_Hhr-1_1_generated_lanelet.xml query_exp1.q -u -D 0.01 -s --max-iterations 1 --eval-runs 1 --good-runs 100 --total-runs 200 --reset-no-better 1 --max-reset-learning 1 >> result_exp1.log
+echo "\nSCENARIO: DEU_Ffb-1_3_T-1\n" >> result_exp1.log
+../uppaal/bin_mac/verifyta ../uppaal/models/DEU_Ffb-1_3_T-1_generated_lanelet.xml query_exp1.q -u -D 0.01 -s --max-iterations 1 --eval-runs 1 --good-runs 100 --total-runs 200 --reset-no-better 1 --max-reset-learning 1 >> result_exp1.log
+echo "\nSCENARIO: DEU_A9-2_1_T-1\n" >> result_exp1.log
+../uppaal/bin_mac/verifyta ../uppaal/models/DEU_A9-2_1_T-1_generated_lanelet.xml query_exp1.q -u -D 0.01 -s --max-iterations 1 --eval-runs 1 --good-runs 100 --total-runs 200 --reset-no-better 1 --max-reset-learning 1 >> result_exp1.log

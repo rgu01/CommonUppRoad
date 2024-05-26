2d63154cf9e7ab69ade9724657b66b5417d8cccf
rgu01 <rong.gu@mdu.se>
2024-05-26 09:06:56 +0200

    animation

--- generate_cr_scenarios.py
+++ generate_cr_scenarios.py
@@ -50,9 +50,47 @@ def make_dynamic_obstacle(obstacle_id, data, w=1.8, l=4.3):
     )
     return obstacle
 
+def create_animation(scenario, planning_problem_set, time):
+    rnd = MPRenderer()
+    dp = MPDrawParams()
+    dp.time_end = time
+    dp.dynamic_obstacle.draw_icon = True
+    dp.dynamic_obstacle.draw_shape = True
+    rnd.create_video([scenario, planning_problem_set], f"experiments/animation/{str(scenario.scenario_id)}_uppaal.gif", draw_params=dp)
+    
+def create_animation2(scenario, planning_problem_set, time):
+    # Render each time step and save as figures
+    for i in range(time):
+        plt.figure(figsize=(25, 10))
+        rnd = MPRenderer(plot_limits=[-20, 125, -25, 125]) # change this to different scenarios
+        rnd.draw_params.time_begin = i
+        scenario.draw(rnd)
+
+        draw_params = DynamicObstacleParams()
+        draw_params.vehicle_shape.occupancy.shape.facecolor = "yellow"
+        draw_params.time_begin = i
+        draw_params.time_end = i + 1
+ 
+        for obstacle in obstacles:
+            obstacle.draw(rnd, draw_params)
+        ego.draw(rnd, draw_params)
+        rnd.render()
+
+        plt.savefig(os.path.join(output_fig_path, f"temp{i}.png"))
+        plt.close()
+
+    # Create a GIF from the saved figures
+    figures_list = [os.path.join(output_fig_path, f"temp{i}.png") for i in range(len(sample))]
+    gif_path = os.path.join(root, f"experiments/animation/{str(scenario.scenario_id)}_uppaal.gif")
+    with imageio.get_writer(gif_path, mode='I') as writer:
+        for filename in figures_list:
+            image = imageio.imread(filename)
+            writer.append_data(image)
+
+
 # Define variables for parsing
 root = os.path.dirname(__file__)
-SCENARIO_PATH = os.path.join(root, 'scenarios/DEU_Ffb-1_3_T-1.xml')
+SCENARIO_PATH = os.path.join(root, 'scenarios/USA_US101-1_1_T-1.xml')
 SAMPLING_LOG_PATH = os.path.join(root, 'uppaal/sampling.log')
 N_OBS_STATES = 5
 N_NEW_OBS = 0
@@ -81,6 +119,8 @@ for obst_states in new_obsts:
     scenario.add_objects(obst)
     obstacles.append(obst)
 
+#scenario.remove_obstacle(scenario.dynamic_obstacles)
+
 ego = make_dynamic_obstacle(scenario.generate_object_id(), ego_states)
 scenario.add_objects(ego)
 obstacles.append(ego)
@@ -89,31 +129,4 @@ obstacles.append(ego)
 output_fig_path = os.path.join(root, "experiments/animation/figures")
 os.makedirs(output_fig_path, exist_ok=True)
 
-# Render each time step and save as figures
-for i in range(len(sample)):
-    plt.figure(figsize=(25, 10))
-    rnd = MPRenderer(plot_limits=[-20, 125, -25, 125]) # change this to different scenarios
-    rnd.draw_params.time_begin = i
-    scenario.draw(rnd)
-
-    draw_params = DynamicObstacleParams()
-    draw_params.vehicle_shape.occupancy.shape.facecolor = "yellow"
-    draw_params.time_begin = i
-    draw_params.time_end = i + 1
-
-    
-    for obstacle in obstacles:
-        obstacle.draw(rnd, draw_params)
-    ego.draw(rnd, draw_params)
-    rnd.render()
-
-    plt.savefig(os.path.join(output_fig_path, f"temp{i}.png"))
-    plt.close()
-
-# Create a GIF from the saved figures
-figures_list = [os.path.join(output_fig_path, f"temp{i}.png") for i in range(len(sample))]
-gif_path = os.path.join(root, f"experiments/animation/{str(scenario.scenario_id)}_uppaal.gif")
-with imageio.get_writer(gif_path, mode='I') as writer:
-    for filename in figures_list:
-        image = imageio.imread(filename)
-        writer.append_data(image)
+create_animation(scenario, planning_problem_set, len(sample))
